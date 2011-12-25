376efb8d0dfa3ee2ee29c13ed60fbbab74c88ea0
Maeyanie <maeyanie@deathwyrm.com>
2011-12-25 17:19:19 -0500

    Bump user-agent version number

--- web.cpp
+++ web.cpp
@@ -36,7 +36,7 @@ int progresscb(void* clientp, double dltotal, double dlnow, double ultotal, doub
 void curlsetup() {
 	curl = curl_easy_init();
 	if (!curl) die("cURL init failed.");
-	curl_easy_setopt(curl, CURLOPT_USERAGENT, "IceCream/0.2 (http://icecream.maeyanie.com)");
+	curl_easy_setopt(curl, CURLOPT_USERAGENT, "IceCream/0.3 (http://icecream.maeyanie.com)");
 	curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, &progresscb);
 	//curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
 }
